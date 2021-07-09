a1df6980573eb4c7d8e6cd7bd45557221cebf340
kalmcam <cam.peele@kalmstudios.com>
2021-07-09 04:07:08 -0400

    Implemented Room and Key spawing

---
+++ Maze/Assets/_Scripts/KeySpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeySpawner : MonoBehaviour
+{
+    public GameObject[] keySpawns;
+    public GameObject keyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keySpawns = GameObject.FindGameObjectsWithTag("Key Spawn");
+
+        int rand = Random.Range(0, keySpawns.Length);
+        GameObject key = Instantiate(keyPrefab, keySpawns[rand].transform.Find("Spawn Point"));
+    }
+}
---
+++ Maze/Assets/_Scripts/RoomSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public GameObject[] roomSpawns;
+    public GameObject roomPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roomSpawns = GameObject.FindGameObjectsWithTag("Room Spawn");
+
+        int rand = Random.Range(0, roomSpawns.Length);
+        GameObject key = Instantiate(roomPrefab, roomSpawns[rand].transform);
+
+        roomSpawns[rand].transform.Find("Wall").gameObject.SetActive(false);
+    }
+}
