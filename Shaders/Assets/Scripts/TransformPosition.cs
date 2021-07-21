c0d15d827e1d8cd0f1a42e5dbf9dd627365dc8b0
kalmcam <cam.peele@kalmstudios.com>
2021-07-21 04:14:04 -0400

    OscillatingCube assignment

---
+++ Shaders/Assets/Scripts/TransformPosition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformPosition : MonoBehaviour
+{
+    public GameObject blueCube;
+    public GameObject yellowCube;
+
+    public Vector3 bluePos;
+    public Vector3 yellowPos;
+
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComputePositions();
+
+        transform.position = Vector3.Lerp(bluePos, yellowPos, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+
+    private void ComputePositions()
+    {
+        bluePos = blueCube.transform.TransformPoint(Vector3.up * 3);
+        yellowPos = yellowCube.transform.TransformPoint(Vector3.down * 3);
+    }
+}
