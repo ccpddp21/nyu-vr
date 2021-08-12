e90ae5878905d626cc3105d00dc255f3ea72b24d
kalmcam <cam.peele@kalmstudios.com>
2021-08-12 15:45:13 -0400

    Imported UpdatedLobby package

---
+++ Profolio/Assets/Scripts/SimpleRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float speed = 0.04f;
+
+    private float _angle = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _angle += speed * Time.deltaTime;
+    }
+}
